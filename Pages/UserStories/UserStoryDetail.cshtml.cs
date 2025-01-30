97cf5156ceb100557f2dcac741d3b790415a0ea1
Jens Peter Andersen <JEAN@zealand.dk>
2025-01-30 23:17:07 +0100

    Modal mechamism added

---
+++ Pages/UserStories/UserStoryDetail.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplicationUserstories2.Pages.UserStories
+{
+    public class UserStoryDetailModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
